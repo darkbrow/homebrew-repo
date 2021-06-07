d4f55c71f9276d636576bac1d25ed73439cc4aa2
Jinbong <darkbrow@gmail.com>
2021-06-08 04:39:41 +0900

    sc-im: 0.8.2 update

--- sc-im.rb
+++ sc-im.rb
@@ -1,10 +1,10 @@
 class ScIm < Formula
   desc "Spreadsheet program for the terminal, using ncurses"
   homepage "https://github.com/andmarti1424/sc-im"
-  url "https://github.com/andmarti1424/sc-im/archive/v0.8.1.tar.gz"
-  sha256 "73958f2adf2548be138f90a1fa2cb3a9c316a6d8d78234ebb1dc408cbf83bac7"
+  url "https://github.com/andmarti1424/sc-im/archive/v0.8.2.tar.gz"
+  sha256 "7f00c98601e7f7709431fb4cbb83707c87016a3b015d48e5a7c2f018eff4b7f7"
   license "BSD-4-Clause"
-  head "https://github.com/andmarti1424/sc-im.git", branch: "freeze"
+  head "https://github.com/andmarti1424/sc-im.git", branch: "main"
 
   depends_on "darkbrow/repo/libxls"
   depends_on "darkbrow/repo/libxlsxwriter"
@@ -12,7 +12,12 @@ class ScIm < Formula
   depends_on "lua"
   depends_on "ncurses"
 
+  uses_from_macos "bison" => :build
   uses_from_macos "libxml2"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
 
   def install
     cd "src" do
