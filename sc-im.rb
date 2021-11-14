4567d6c6898ee74b0f102c5afaf9ec6532b2c0d5
Jinbong <darkbrow@gmail.com>
2021-11-14 15:35:24 +0900

    sc-im: change branch to main

--- sc-im.rb
+++ sc-im.rb
@@ -4,15 +4,16 @@ class ScIm < Formula
   url "https://github.com/andmarti1424/sc-im/archive/v0.8.2.tar.gz"
   sha256 "7f00c98601e7f7709431fb4cbb83707c87016a3b015d48e5a7c2f018eff4b7f7"
   license "BSD-4-Clause"
-  revision 1
-  head "https://github.com/andmarti1424/sc-im.git", branch: "dev"
+  revision 3
+  head "https://github.com/andmarti1424/sc-im.git", branch: "main"
 
   bottle do
-    sha256 arm64_big_sur: "fb1822b15198f453e30271bba682a99f691cc40d06a6b579c7a6f7dcc2d369c0"
-    sha256 big_sur:       "73d779f46ea650c13122071a3ffe42c74c041ddc2cba44a7dfcffdf165675e54"
-    sha256 catalina:      "27ba7d0efc18b31798bb241bb8715e3a27c12e85fb6d3d32d71d86d7f238033a"
-    sha256 mojave:        "dfe4dcccf9298bfef23dbb5202ff4cc271146df186ce34b211d569bae8fe7f84"
-    sha256 x86_64_linux:  "8bf0655f44d0c532ef10a05fc234361fd99dbea75919399f0cf5ca98d2718fae"
+    sha256 arm64_monterey: "7ebc2e0f8248b991474defdab0519dbffc74761b0627100abb9a52e15cc1f945"
+    sha256 arm64_big_sur:  "7a695c6f3c7c830c2c88bf60ec0bc8e844a82b1adf7ed4cd474d89326c0600ff"
+    sha256 monterey:       "16d81d91ba10cc86b39c3408290a3dfc71458c0e72b9dce4bf0dde9a817600f6"
+    sha256 big_sur:        "311253002c6a2e14f2003a7e7e8f88ecbe54a7bd6f373a695e14a2cb4ec0a377"
+    sha256 catalina:       "d25892c33ee8ac59c5e6439a9fe7893fa49c4a8514c2aac880f95f997c3eef32"
+    sha256 x86_64_linux:   "cfcf853d84da9ede68d92fed791600c887869a2fcb2c689eafb3f491a00b06af"
   end
 
   depends_on "darkbrow/repo/libxls"
