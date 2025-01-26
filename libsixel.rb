660103e994321bbd131e78e7dfa50ff6abecde99
darkbrow <darkbrow@gmail.com>
2025-01-27 06:15:26 +0900

    Update libsixel.rb

--- libsixel.rb
+++ libsixel.rb
@@ -1,9 +1,9 @@
 class Libsixel < Formula
   desc "SIXEL encoder/decoder implementation"
   homepage "https://github.com/saitoha/sixel"
-  url "https://github.com/libsixel/libsixel/archive/refs/tags/v1.10.3.tar.gz"
-  sha256 "028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0"
-  revision 1
+  url "https://github.com/libsixel/libsixel/archive/refs/tags/v1.10.5.tar.gz"
+  sha256 "b6654928bd423f92e6da39eb1f40f10000ae2cc6247247fc1882dcff6acbdfc8"
+  license "MIT"
   head "https://github.com/libsixel/libsixel.git", branch: "master"
 
   depends_on "meson" => :build
@@ -14,7 +14,6 @@ class Libsixel < Formula
 
   def install
     system "meson", "setup", "build", "-Dgdk-pixbuf2=disabled", "-Dtests=disabled", *std_meson_args
-    # system "meson", *std_meson_args, "build", "-Dgdk-pixbuf2=disabled", "-Dtests=disabled"
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
